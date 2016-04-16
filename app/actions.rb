e05c2ebc6c05d2cf47a41845aa21880bfe2b9c12
vinoscript <wstikk@gmail.com>
2016-04-16 03:37:11 +0000

    Step 6 Message done

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,19 @@ get '/messages' do
   @messages = Message.all
   erb :'messages/index'
 end
+
+post '/messages' do
+  binding.pry
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+  @message.save
+  redirect '/messages'
+end
+
+get '/messages/new' do
+  erb :'messages/new'
+end
+
