4070f86bf491ece135fa2379c2072baf68c51ca9
vinoscript <wstikk@gmail.com>
2016-04-16 21:11:01 +0000

    URL added - partial functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416004756) do
+ActiveRecord::Schema.define(version: 20160416195402) do
 
   create_table "messages", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.string "content"
+    t.string   "author"
+    t.string   "content"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
